e76592cce116cf6cf4a63064c5e07bcc4d58dacd
nzigovic <nadirzigovic4@gmail.com>
2024-12-14 17:56:43 +0100

    Cas 1 zavrsen

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$ime = "Nadir";
+$prezime = "Zigovic";
+$godine = 21;
+$visina = 180;
+
+echo "Moje ime je $ime prezivam se $prezime imam $godine godina i visok sam $visina"; 
---
+++ vezba.php
@@ -0,0 +1,10 @@
+<?php
+
+    $cena = 500;
+    $porez = 0.22;
+    $saporezom = $cena*$porez+$cena; 
+
+    echo "cena bez porza je $cena, sa porezom je $saporezom";
+    
+
+?>
