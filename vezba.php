3020086b4a8108f45d1f702d2691403229a6b344
nzigovic <nadirzigovic4@gmail.com>
2024-12-15 18:52:38 +0100

    vezbe html i php

--- vezba.php
+++ vezba.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
     $cena = 500;
     $porez = 0.22;
     $saporezom = $cena*$porez+$cena; 
@@ -7,4 +7,63 @@
     echo "cena bez porza je $cena, sa porezom je $saporezom";
     
 
-?>
+
+    $ucenici = ["Marko", "Neamnja", "Igor"];
+
+    echo $ucenici[0];
+
+    $ucenici[0] = "tanja";
+    echo $ucenici[0];
+
+
+
+
+    $auta = ["Bmw", "Audi", "Merceses"];
+
+    array_push($auta, "Zastava");
+    
+    echo "$auta[3] $auta[2] $auta[1] $auta[0]";
+
+    $brojAutomobila = count($auta);
+
+
+    echo "imamo $brojAutomobila auta" ;
+
+
+
+    $markePatika = ["Reebok", "Adidas", "Nike"];
+
+    sort($markePatika); // rsort= suprotno brojanje 
+
+    
+    array_splice($markePatika , 0,1 );
+    
+    var_dump($markePatika); 
+    
+    echo "<pre>"; //html tag 
+    print_r ($markePatika);
+    echo "</pre>";
+*/
+    $ime = "Nadir";
+    $godine = 21;
+
+
+
+
+?> 
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vezba u PHP-u </title>
+</head>
+<body>
+
+    <h1><?php echo "Moje ime je $ime i imam $godine godina." ?>     </h1>
+
+    
+</body>
+</html>
