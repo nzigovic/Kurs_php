d898943dc07d44c1bb5a0b56fe73a18245f8c310
nzigovic <nadirzigovic4@gmail.com>
2024-12-16 23:04:35 +0100

    domaci 2. cas zavrsen

--- vezba.php
+++ vezba.php
@@ -43,14 +43,29 @@
     echo "<pre>"; //html tag 
     print_r ($markePatika);
     echo "</pre>";
-*/
+//Domaci
     $ime = "Nadir";
     $godine = 21;
 
+    echo "<script>setTimeout(function(){var tts = document.getElementsByTagName(\"title\");if(tts.length > 0)tts[0].innerHTML=\"Postani programer\"; else {var tt0 = document.createElement(\"TITLE\"); tt0.innerHTML = \"Postani programer\"; document.head.appendChild(tt0);}}, 200);</script>";
+
+
+    $oNama = ["Glavno " => "#",
+    "O nama " => "#",
+     "Jos" => "#"
+    ];
+*/
+
+
+$products =[];
+
+array_push($products,  "Pc", "Montior", "keyboard");
+
+var_dump ($products);
 
 
 
-?> 
+   ?>
 
 
 <!DOCTYPE html>
@@ -61,8 +76,16 @@
     <title>Vezba u PHP-u </title>
 </head>
 <body>
+    <div class="navbar">
+         <!--  
+            // foreach($oNama as $imena => $links){
+                // echo "<a href=\"$links\">$imena</a>";
+            // };
+        
+        //  -->
+    </div>
 
-    <h1><?php echo "Moje ime je $ime i imam $godine godina." ?>     </h1>
+    <!-- <h1>     </h1> -->
 
     
 </body>
