789cb3bb4a39002f7a102cab45e3b9226997209e
nzigovic <nadirzigovic4@gmail.com>
2025-01-09 22:17:34 +0100

    domaci je zavrsen za cas 6

--- Cas 6/vezba.php
+++ Cas 6/vezba.php
@@ -1,12 +1,21 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $pin = $_POST['pin'] ?? ''; // Provera da li je 'pin' postavljen
+if (isset($_POST["pin"])) {
+    $pin = $_POST['pin']; 
 
-    if (strlen($pin) < 4) {
-        echo "Unesite najmanje 5 karaktera.";
-    } else {
+    if ($pin < 4) {
+        echo "Unesite najmanje 4 karaktera.";
+    }else if($pin > 6){
+        echo "maximum je 6 karaktera";
+    }
+    else {
         echo "PIN je validan.";
     }
-}
+};
+
+
+
+
+
 ?>
+
 
