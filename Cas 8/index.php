d4f652de18d7e0866b11b864392340bb1543a69f
nzigovic <nadirzigovic4@gmail.com>
2025-01-19 20:25:19 +0100

    vezba 8,4

--- Cas 8/index.php
+++ Cas 8/index.php
@@ -26,9 +26,44 @@ $automobili = [
 
     "Toyota" =>[
         "model" => "Rav4",
-        "godiste" => 2014
+        "godiste" => 2019
     ]
 
 ];
+
+    
+
+foreach ($automobili as $marka => $auto) {
+
+    $godiste = $auto["godiste"];
+    $trenutaGodina = date("Y");
+
+    $sracunateGodine = $trenutaGodina - $godiste;
+
+    echo $sracunateGodine . " godine <br>";
+
+    if ($sracunateGodine <=5){
+        echo "Vas auto je novo";
+    }
+
+
+}
+
+
+
+foreach ($automobili  as $model => $auto  ) {
+    $godiste = $auto["godiste"];
+
+    $trenutanDatum = date("Y");
+    $godine = $trenutanDatum - $godiste;
+    echo "Model: " . $model . "<br> Godine: " . $godine . "<br>";
+    if ($godine <= 5 ){
+        echo "Model auta je $model i auto nije star od 5 godina";
+    }elseif ($godine > 5 && $godine <= 10){
+        echo "vase auto je starije od 5 godina";
+    }
+};
+
+
 
 
