75ae92e393a1b39ac709c3007b4cfd41b698e8ce
nzigovic <nadirzigovic4@gmail.com>
2025-01-23 20:20:06 +0100

    test da li se je sve vratilo?

---
+++ Cas 10/index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    function pomnoziDvaBroja($broj1, $broj2)
+    {
+        $rezultat =  $broj1 * $broj2;
+        return $rezultat;
+    }
+
+   $pomnozeniBrojevi= pomnoziDvaBroja(6,6);
+
+   echo $pomnozeniBrojevi;
+
+
+   echo "21313";
