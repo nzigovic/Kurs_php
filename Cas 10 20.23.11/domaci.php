77358b00159ca041752d68e2ee236540beda0324
nzigovic <nadirzigovic4@gmail.com>
2025-01-29 21:31:08 +0100

    doamci

--- Cas 10 20.23.11/domaci.php
+++ Cas 10 20.23.11/domaci.php
@@ -1,34 +1,45 @@
 <?php
 
 // cena dostave - 100km 200din 100-200km 350din, preko 500din///
+function izracunajDostavu($cena, $grad)
+{
+    $cenaDostave = 0;
+    $dostava = [
 
+        "Subotica" => 220,
+        "Pancevo" => 10,
+        "Sarajevo" => 292,
+        "Split" => 799
+    ];
 
+    $gradPostoji = array_key_exists($grad, $dostava);
+    if($gradPostoji)
+    {
+        $rastojanje = $dostava[$grad];
+        if($rastojanje <= 100)
+        {
+            $cenaDostave = 200;
+        }
+        else if($rastojanje > 100 && $rastojanje <= 200)
+        {
+            $cenaDostave = 350;
+        }
+        else {
+            $cenaDostave = 500;
+        }
+    }
+    else
+    {
+        $cenaDostave = null;
+    }
 
-
-function rastojanje ($cena, $adresa){
-
-
-
-
-
+    return $cenaDostave;
 }
 
+$pancevo = izracunajDostavu(2000, "Pancevo");
+izracunajDostavu(2000, "Sarajevo");
+izracunajDostavu(2000, "Split");
+$cuprija = izracunajDostavu(2000, "Cuprija");
 
 
-
-$dostava = [
-    "Subotica" => 220,
-    "Pancevo" => 10,
-    "Sarajevo" => 292,
-    "Spit" => 799
-];
-
-
-foreach ($dostava as $grad => $udaljenost ){
-    echo "$grad";
-
-}
-
-
-
-var_dump($dostava);
+echo $cuprija;
