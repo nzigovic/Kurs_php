f5a15f217010d4524b8ad57eff88712b9d963705
nzigovic <nadirzigovic4@gmail.com>
2025-02-06 14:15:37 +0100

    ubacivanje u sql

--- Cas 13/index.php
+++ Cas 13/index.php
@@ -8,6 +8,14 @@
         die ("Ne mozemo da se povezemo");
     }
 
-    mysqli_query($baza, "INSERT INTO korisnici (email, lozinka, datum_rodjenja) VALUES ('testizphpa@gmai.com', 'admin214314','1999-2-12' )");
+//    mysqli_query($baza, "INSERT INTO korisnici (email, lozinka, datum_rodjenja) VALUES ('testizphpa@gmai.com', 'admin214314','1999-2-12' )");
 
-    
+    $ime= "krompir";
+    $opis = "beli kropmpir";
+    $cena = 100;
+    $dan_nabavke = "2021-02-12";
+    $kolicina = 10;
+
+    $baza -> query("INSERT INTO proizvodi (ime, opis, cena, dan_nabavke, kolicina)   VALUES ('$ime', '$opis', $cena, '$dan_nabavke', $kolicina)");
+
+
