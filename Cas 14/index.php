3f4a49b3776fa2e881879252cfcb0d53c481c010
nzigovic <nadirzigovic4@gmail.com>
2025-02-26 22:39:48 +0100

    cas 14 zapocet

--- Cas 14/index.php
+++ Cas 14/index.php
@@ -10,3 +10,19 @@ if (mysqli_connect_error()) {
 
 
 
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+<input type=" ">
+
+</body>
+</html>
+
