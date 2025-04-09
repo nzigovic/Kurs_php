e6d79316b9d0045ff517e60002db986cad2f136f
nzigovic <nadirzigovic4@gmail.com>
2025-04-09 14:00:18 +0200

    domaci 14 zavrsen

--- Cas 14/domaci.php
+++ Cas 14/domaci.php
@@ -1,13 +1,29 @@
 <?php
 
-require_once "baza.php";
 
 
+    if(!isset($_POST['email'])|| empty($_POST['email'])) {
 
-    if(!isset($_POST['email'])) {
 
     die("Niste uneli Email!");
 
     };
 
 
+
+    if(!isset($_POST['sifra'])|| empty($_POST['sifra'])) {
+
+
+        die("Niste uneli sifru!");
+
+    };
+
+
+    $email = $_POST['email'];
+    $password = $_POST['sifra'];
+
+
+    require_once "baza.php";
+
+
+    $baza->query("INSERT INTO korisnici (email, sifra) VALUES ('$email', '$password')");
