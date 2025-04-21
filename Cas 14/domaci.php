6fd1f7dcbd850ad27a46009d06c407679c8b8d2d
nzigovic <nadirzigovic4@gmail.com>
2025-04-21 09:19:11 +0200

    cas 15 lekcija 15.12

--- Cas 14/domaci.php
+++ Cas 14/domaci.php
@@ -1,36 +1,42 @@
 <?php
+    if (!isset($_POST['email']) || empty($_POST['email'])) {
+        die("Niste uneli Email!");
+    }
 
-
-
-    if(!isset($_POST['email'])|| empty($_POST['email'])) {
-
-
-    die("Niste uneli Email!");
-
-    };
-
-
-
-    if(!isset($_POST['sifra'])|| empty($_POST['sifra'])) {
-
-
-        die("Niste uneli sifru!");
-
-    };
-
+    if (!isset($_POST['sifra']) || empty($_POST['sifra'])) {
+        die("Niste uneli šifru!");
+    }
 
     $email = $_POST['email'];
-
     $password = password_hash($_POST['sifra'], PASSWORD_BCRYPT);
-    var_dump($password); exit();
-
-
 
     require_once "baza.php";
 
+    // Proveri da li već postoji korisnik sa tim emailom
+    $provera = $baza->prepare("SELECT id FROM korisnici WHERE email = ?");
+    $provera->bind_param("s", $email);
+    $provera->execute();
+    $provera->store_result();
 
-    $baza->query("INSERT INTO korisnici (email, sifra) VALUES ('$email', '$password')");
+    if ($provera->num_rows > 0) {
+        // Korisnik već postoji
+        echo "Već ste registrovani sa ovim emailom!";
+        $provera->close();
+        $baza->close();
+        exit();
+    }
+    $provera->close();
+
+    $stmt = $baza->prepare("INSERT INTO korisnici (email, sifra) VALUES (?, ?)");
 
 
+     $stmt->bind_param("ss", $email, $password);
 
-    echo "test";
+    if ($stmt->execute()) {
+        echo "Uspešno ste registrovani!";
+    } else {
+        echo "Greška prilikom registracije: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $baza->close();
---
+++ Cas 15/pretraga.php
@@ -0,0 +1,24 @@
+<?php
+
+
+    require_once "baza.php";
+
+
+    $email = $_POST['email'];
+
+    $provera = $baza->prepare("SELECT * FROM korisnici WHERE email = ?");
+    $provera->bind_param("s", $email);
+    $provera->execute();
+    $provera->store_result();
+
+    if ($provera-> num_rows > 0) {
+    echo "Nasli smo korisnika u bazi";
+        $provera->close();
+        $baza->close();
+    exit;
+
+
+    }  else {
+      echo "Nema korisnika!";}
+
+    $provera ->close();
