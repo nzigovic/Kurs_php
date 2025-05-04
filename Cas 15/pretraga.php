ebb420c88b7a35e867cc5aa80ee1e4478394e089
Nadi <zigovicnadir03@gmail.com>
2025-05-04 20:01:30 +0200

    funkcija za proveru korisnika sa slov..om

--- Cas 15/pretraga.php
+++ Cas 15/pretraga.php
@@ -3,46 +3,98 @@
 
     require_once "baza.php";
 
+/*
+    $email = $_POST['email'];
 
-//    $email = $_POST['email'];
+    $provera = $baza->prepare("SELECT * FROM korisnici WHERE email = ?");
+    $provera->bind_param("s", $email);
+    $provera->execute();
+    $provera->store_result();
 
-//    $provera = $baza->prepare("SELECT * FROM korisnici WHERE email = ?");
-//    $provera->bind_param("s", $email);
-//    $provera->execute();
-//    $provera->store_result();
-//
-//    if ($provera-> num_rows > 0) {
-//    echo "Nasli smo korisnika u bazi!";
-//        $provera->close();
-//        $baza->close();
-//    exit;
-//
-//
-//    }  else {
-//      echo "Korisnik nije pronadjen!";}
-//
-//    $provera ->close();
+    if ($provera-> num_rows > 0) {
+    echo "Nasli smo korisnika u bazi!";
+        $provera->close();
+        $baza->close();
+    exit;
 
 
+    }  else {
+      echo "Korisnik nije pronadjen!";}
 
-function ProverIKorisnikaSaSlovom($baza, $domen)
+    $provera ->close();*/
+
+
+/*
+function ProveriKorisnikaSaSlovom($baza, $domen)
 {
-
-    $upit = $pdo->prepare("SELECT COUNT (*) FROM korisnici WHERE email LIKE ? ");
-    $pattent = "%". $domen;
+    $upit = $baza->prepare("SELECT COUNT(*) FROM korisnici WHERE email LIKE ?");
+    $pattern = "%" . $domen;
     $upit->bind_param("s", $pattern);
     $upit->execute();
     $upit->bind_result($broj);
     $upit->fetch();
     $upit->close();
 
-
     return $broj > 0;
 };
 
-if (ProverIKorisnikaSaSlovom($baza , '@gmail.com')
+if (ProveriKorisnikaSaSlovom($baza , '@gmail.com')) {
+    echo "Korisnik postoji";
+} else {
+    echo "Korisnik ne postoji";
+}*/
 
-{
+
+
+/*
+function SviSaPosebinimImenom ($baza, $domen){
+
+        $upit = $baza->prepare("SELECT * FROM korisnici WHERE email LIKE ?");
+        $pattern = "%" . $domen;
+        $upit->bind_param("s", $pattern);
+        $upit->execute();
+        $rezultat = $upit->get_result();
+
+        $korisnici = [];
+
+    while ($red = $rezultat->fetch_assoc()) {
+        $korisnici[] = $red;
+    };
+
+    return $korisnici;
+
 }
 
-)
+$korisnici = DajKorisnikeSaDomenom($baza, '@gmail.com');
+
+if (count($korisnici) > 0) {
+    echo "Korisnici sa domenom @gmail.com:<br>";
+    foreach ($korisnici as $korisnik) {
+        echo "ID: " . $korisnik['id'] . " - Email: " . $korisnik['email'] . "<br>";
+    }
+} else {
+    echo "Nema korisnika sa datim domenom.";
+}*/
+
+
+
+function DajKorisnikeSaDomenom($baza, $domen) {
+    $upit = $baza->prepare("SELECT * FROM korisnici WHERE email LIKE ?");
+    $pattern = "%" . $domen;
+    $upit->bind_param("s", $pattern);
+    $upit->execute();
+    $rezultat = $upit->get_result();
+
+    $korisnici = [];
+
+    while ($red = $rezultat->fetch_assoc()) {
+        $korisnici[] = $red;
+    }
+
+    $upit->close();
+
+    return $korisnici;
+}
+
+$korisnici = DajKorisnikeSaDomenom($baza, '@gmail.com');
+
