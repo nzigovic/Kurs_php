0fdd2e107f438b3ac6a1a45b1a0d92c3666e2b49
nzigovic <zigovicnadir03@gmail.com>
2025-05-17 23:19:12 +0200

    Pravljenje web shopa i logika za povlacnjnje iz baze

--- Cas 16/index.php
+++ Cas 16/index.php
@@ -1,1 +1,35 @@
 <?php
+        require_once "modeli/baza.php";
+
+        $rezultat = $baza->query("SELECT * FROM proizvodi");
+        $proizvodi = $rezultat->fetch_all(MYSQLI_ASSOC);
+
+
+
+
+
+
+
+    ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Proizvodi</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php foreach ($proizvodi as $proizvod): ?>
+    <div class="card">
+        <h2><?= $proizvod['ime_proizvoda'] ?></h2>
+        <p><?= $proizvod['opis'] ?></p>
+        <p class="cena">Cena: <?= $proizvod['cena'] ?> €</p>
+       <? if((int) $proizvod['kolicina']  ): ?>
+        <p class="na-stanju">Ima na stanju (<?= $proizvod['kolicina'] ?> komada)</p>
+        <? else: ?>
+        <p>Nema na stanju </p>
+    </div>
+<?php endforeach; ?>
+</body>
+</html>
