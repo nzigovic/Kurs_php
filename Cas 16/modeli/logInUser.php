ada119f8549289c1671bcb1a777bd53f3d8f61bf
nzigovic <zigovicnadir03@gmail.com>
2025-05-18 18:45:54 +0200

    website log in forma i user provera za adminaaa

--- Cas 16/modeli/logInUser.php
+++ Cas 16/modeli/logInUser.php
@@ -1,27 +1,46 @@
 <?php
+$poruka = "";
 
-
-
-    if(!isset($_POST["email"]) || empty($_POST["email"])){
-        die("unesite email");
-    }
-
-    if(!isset($_POST["sifra"]) || empty($_POST["sifra"])){
-        die("unesite sifra");
-    }
-
-
+if (!isset($_POST["email"]) || empty($_POST["email"])) {
+    $poruka = "Unesite email";
+} elseif (!isset($_POST["sifra"]) || empty($_POST["sifra"])) {
+    $poruka = "Unesite šifru";
+} else {
     require_once "baza.php";
 
     $email = $_POST["email"];
     $sifra = $_POST["sifra"];
 
-        $rezultat = $baza->query("SELECT * FROM korisnici WHERE email = '$email'");
+    $rezultat = $baza->query("SELECT * FROM korisnici WHERE email = '$email'");
 
-        if($rezultat->num_rows == 1) {
-           $korisnik = $rezultat->fetch_assoc();
-        var_dump($korisnik);
-        }
-      else{
-        echo "Korisnik ne postoji";
-      }
+    if ($rezultat->num_rows == 1) {
+        $korisnik = $rezultat->fetch_assoc();
+        $poruka = "Dobro došli, Admine!";
+    } else {
+        $poruka = "Korisnik ne postoji";
+    }
+}
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <title>Login Status</title>
+</head>
+<body>
+<div class="container">
+    <?php if (!empty($poruka)): ?>
+        <div class="poruka <?= ($poruka === 'Dobro došli, Admine!') ? 'uspeh' : 'greska' ?>">
+            <?= htmlspecialchars($poruka) ?>
+        </div>
+    <?php endif; ?>
+</div>
+</body>
+</html>
