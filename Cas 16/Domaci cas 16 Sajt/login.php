e84be19920623d4df9e5622dadec37b82ce08017
nzigovic <zigovicnadir03@gmail.com>
2025-05-20 21:22:12 +0200

    domaci 16 u toku za registraciju

--- Cas 16/Domaci cas 16 Sajt/login.php
+++ Cas 16/Domaci cas 16 Sajt/login.php
@@ -1,3 +1,31 @@
 <?php
 
-        
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Admin</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div class="login-container">
+    <form action="loginForma.php" method="post" class="login-form">
+        <h2>Prijava</h2>
+        <input type="email" name="email" placeholder="Unesite vaš email" >
+        <input type="password" name="sifra" placeholder="Unesite vašu lozinku" >
+        <button type="submit">Uloguj me</button>
+
+        <h4>Ulogujte se kao <a href="#">User</a> </h4>
+    </form>
+</div>
+</body>
+</html>
---
+++ Cas 16/Domaci cas 16 Sajt/loginForma.php
@@ -0,0 +1,35 @@
+<?php
+
+
+    if (!isset($_POST["email"])  || empty($_POST["email"])){
+
+        die("Polje email nije popunjeno");
+    }
+
+    if (!isset($_POST["sifra"])  || empty($_POST["sifra"])){
+
+        die("Polje sifra nije popunjeno");
+    }
+
+
+    require_once "baza.php";
+
+    $email =  $_POST['email'];
+    $password = password_hash($_POST['sifra'], PASSWORD_DEFAULT);
+
+
+
+    $provera = $baza -> prepare("select id from korisnici where email = ?");
+
+    $provera -> bind_param( "s", $email);
+    $provera -> execute();
+    $provera -> store_result();
+
+
+    if($provera ->num_rows > 0){
+        echo "korisnik postoji";
+        $provera -> close();
+        $baza -> close();
+        exit();
+    }
+    $provera -> close();
