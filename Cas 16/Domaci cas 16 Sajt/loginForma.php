a0e4b354e71179ec7e4a97d1037f79fb34598d02
nzigovic <nadirzigovic4@gmail.com>
2025-05-23 23:26:45 +0200

    mala prepravka

--- Cas 16/Domaci cas 16 Sajt/loginForma.php
+++ Cas 16/Domaci cas 16 Sajt/loginForma.php
@@ -33,3 +33,21 @@ if ($rezultat->num_rows === 1) {
 $provera->close();
 $baza->close();
 
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
